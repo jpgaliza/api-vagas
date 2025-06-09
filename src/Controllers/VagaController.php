d918e1efe3e11cbb13d38947e202f489540aa31c
João Pedro <jpfgaliza@gmail.com>
2025-06-09 15:48:32 -0300

    Endpoint POST /vagas - Ajuste finais da criação de vagas

--- src/Controllers/VagaController.php
+++ src/Controllers/VagaController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Controllers;
-
 use App\Models\Vaga;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
--- src/Models/Vaga.php
+++ src/Models/Vaga.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Models;
-
 use App\Database\Database;
 use App\Utils\Validator;
 use PDO;
